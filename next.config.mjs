014a76f9c57542e6eeb90a7a711fd042d9bad1a1
Multyp <miguel.dasilva2004@hotmail.com>
2024-06-26 07:32:39 -0400

    del: gh-pages removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/stormhold-world',
-    assetPrefix: '/stormhold-world',
+    assetPrefix: '/stormhold-world/',
     output: "export",
 };
 
