baa430a3122afc853e283dc0516decd14714ba5e
Multyp <miguel.dasilva2004@hotmail.com>
2024-06-29 20:46:46 -0400

    fix: images links in locations pages

--- src/app/locations/locations.ts
+++ src/app/locations/locations.ts
@@ -3,35 +3,35 @@ const locations = [
     name: "LARIOM",
     description:
       "The human kingdom situated in the southwest of the continent, known for its strategic military positions and rich cultural history.",
-    imageSrc: "/assets/LARIOM.jpeg",
+    imageSrc: "/stormhold-world/assets/LARIOM.jpeg",
     link: "/locations/lariom",
   },
   {
     name: "DAWNFALL",
     description:
       "A dwarven kingdom located in the northwest, built atop THE GREAT MINES which are the primary source of stones and metals for the kingdom.",
-    imageSrc: "/assets/DAWNFALL.webp",
+    imageSrc: "/stormhold-world/assets/DAWNFALL.webp",
     link: "/locations/dawnfall",
   },
   {
     name: "MIRLUN",
     description:
       "An independent elven kingdom known for its ancient forests and mystical heritage. Although it operates under its own local governance, it respects the laws set by THE GREAT ELDERS.",
-    imageSrc: "/assets/MIRLUN.webp",
+    imageSrc: "/stormhold-world/assets/MIRLUN.webp",
     link: "/locations/mirlun",
   },
   {
     name: "SHUBORE",
     description:
       "A coastal city known for its bustling trade ports and vibrant cultural festivals. It serves as a major hub for commerce and cultural exchange in the southern region.",
-    imageSrc: "/assets/SHUBORE.webp",
+    imageSrc: "/stormhold-world/assets/SHUBORE.webp",
     link: "/locations/shubore",
   },
   {
     name: "GOBLIN FORESTS",
     description:
       "Dense forests in the northeast, populated mostly by goblins and wolves, recognized as an independent region under goblin control.",
-    imageSrc: "/assets/GOBLIN_FORESTS.webp",
+    imageSrc: "/stormhold-world/assets/GOBLIN_FORESTS.webp",
     link: "/locations/goblin_forests",
   },
 ];
