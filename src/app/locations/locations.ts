f30eb77fdc3990e1861f643ac22796e7f7b2aab8
Multyp <miguel.dasilva2004@hotmail.com>
2024-06-30 13:58:31 -0400

    edit: added tag system to locations

--- src/app/locations/locations.ts
+++ src/app/locations/locations.ts
@@ -1,38 +1,45 @@
+import imageUrls from "@/constants/imageUrls";
+
 const locations = [
   {
     name: "LARIOM",
     description:
       "The human kingdom situated in the southwest of the continent, known for its strategic military positions and rich cultural history.",
-    imageSrc: "/stormhold-world/assets/LARIOM.webp",
+    imageSrc: imageUrls.lariom,
     link: "/locations/lariom",
+    tags: ["human", "kingdom"],
   },
   {
     name: "DAWNFALL",
     description:
       "A dwarven kingdom located in the northwest, built atop THE GREAT MINES which are the primary source of stones and metals for the kingdom.",
-    imageSrc: "/stormhold-world/assets/DAWNFALL.webp",
+    imageSrc: imageUrls.dawnfall,
     link: "/locations/dawnfall",
+    tags: ["dwarven", "kingdom"],
   },
   {
     name: "MIRLUN",
     description:
       "An independent elven kingdom known for its ancient forests and mystical heritage. Although it operates under its own local governance, it respects the laws set by THE GREAT ELDERS.",
-    imageSrc: "/stormhold-world/assets/MIRLUN.webp",
+    imageSrc: imageUrls.mirlun,
     link: "/locations/mirlun",
+    tags: ["elven", "kingdom", "forest"],
   },
   {
     name: "SHUBORE",
     description:
       "A coastal city known for its bustling trade ports and vibrant cultural festivals. It serves as a major hub for commerce and cultural exchange in the southern region.",
-    imageSrc: "/stormhold-world/assets/SHUBORE.webp",
+    imageSrc: imageUrls.shubore,
     link: "/locations/shubore",
+    tags: ["coastal", "kingdom"],
   },
   {
     name: "GOBLIN FORESTS",
     description:
       "Dense forests in the northeast, populated mostly by goblins and wolves, recognized as an independent region under goblin control.",
-    imageSrc: "/stormhold-world/assets/GOBLIN_FORESTS.webp",
+    imageSrc: imageUrls.goblinForests,
     link: "/locations/goblin_forests",
+    tags: ["goblins", "forest"],
   },
 ];
 
