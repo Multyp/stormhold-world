fa1afbd0014f245ac2c48a8280835aa8f0c8ffca
Multyp <miguel.dasilva2004@hotmail.com>
2024-06-30 00:16:04 -0400

    feat: updated images urls with "dev" links with env and updated world data

--- src/constants/imageUrls.ts
+++ src/constants/imageUrls.ts
@@ -1,19 +1,36 @@
 const imageUrls = {
-  default: "assets/UNKNOWN.jpg",
+  default:
+    process.env.NEXT_PUBLIC_IMAGE_URL_DEFAULT ||
+    "/stormhold-world/assets/UNKNOWN.jpg",
+
   /*
     LOCATIONS
   */
-  lariom: "/assets/LARIOM.webp",
-  dawnfall: "/assets/DAWNFALL.webp",
-  mirlun: "/assets/MIRLUN.webp",
-  shubore: "/assets/SHUBORE.webp",
-  stormhold: "/assets/STORMHOLD.webp",
-  goblinForests: "/assets/GOBLIN_FORESTS.webp",
+  lariom:
+    process.env.NEXT_PUBLIC_IMAGE_URL_LARIOM ||
+    "/stormhold-world/assets/LARIOM.webp",
+  dawnfall:
+    process.env.NEXT_PUBLIC_IMAGE_URL_DAWNFALL ||
+    "/stormhold-world/assets/DAWNFALL.webp",
+  mirlun:
+    process.env.NEXT_PUBLIC_IMAGE_URL_MIRLUN ||
+    "/stormhold-world/assets/MIRLUN.webp",
+  shubore:
+    process.env.NEXT_PUBLIC_IMAGE_URL_SHUBORE ||
+    "/stormhold-world/assets/SHUBORE.webp",
+  stormhold:
+    process.env.NEXT_PUBLIC_IMAGE_URL_STORMHOLD ||
+    "/stormhold-world/assets/STORMHOLD.webp",
+  goblinForests:
+    process.env.NEXT_PUBLIC_IMAGE_URL_GOBLIN_FORESTS ||
+    "/stormhold-world/assets/GOBLIN_FORESTS.webp",
 
   /*
     CHARACTERS
   */
-  typhaniaMelith: "/assets/characters/Mirlun/typhania_melith.jpg",
+  typhaniaMelith:
+    process.env.NEXT_PUBLIC_IMAGE_URL_TYPHANIA_MELITH ||
+    "/stormhold-world/assets/characters/Mirlun/typhania_melith.jpg",
 };
 
 export default imageUrls;
