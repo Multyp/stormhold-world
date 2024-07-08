5e70964df43a1be534cebc427d72dab0bd444f24
Multyp <miguel.dasilva2004@hotmail.com>
2024-07-08 13:02:09 -0400

    feat: added image modal on galleries + updated section image to non-draggable

--- src/components/base/SectionGallery.tsx
+++ src/components/base/SectionGallery.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import imageUrls from "@/constants/imageUrls";
-import React from "react";
+import React, { useState } from "react";
+import ImageModal from "@/components/base/ImageModal";
 
 interface GalleryImage {
   url: string;
@@ -12,10 +15,23 @@ interface SectionGalleryProps {
 }
 
 const SectionGallery: React.FC<SectionGalleryProps> = ({ gallery }) => {
+  const [selectedImage, setSelectedImage] = useState<GalleryImage | null>(null);
+
+  const handleImageClick = (image: GalleryImage) => {
+    setSelectedImage(image);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div className="text-lg text-gray-600 mt-4 text-left max-w-screen-lg">
       <div className="grid grid-cols-2 gap-4 mt-4 max-h-96 h-full">
-        <div className="relative col-span-1">
+        <div
+          className="relative col-span-1 cursor-pointer"
+          onClick={() => handleImageClick(gallery[0])}
+        >
           <Image
             src={imageUrls[gallery[0].url as keyof typeof imageUrls]}
             alt={gallery[0].alt}
@@ -23,6 +39,7 @@ const SectionGallery: React.FC<SectionGalleryProps> = ({ gallery }) => {
             style={{ height: "100%" }}
             width={0}
             height={0}
+            draggable={false}
           />
           <div className="absolute bottom-2 right-2 bg-black text-white text-sm px-2 py-1 rounded-lg opacity-75">
             AI Generated
@@ -32,7 +49,8 @@ const SectionGallery: React.FC<SectionGalleryProps> = ({ gallery }) => {
           {gallery.slice(1).map((image, index) => (
             <div
               key={index}
-              className="relative rounded-md overflow-hidden"
+              className="relative rounded-md overflow-hidden cursor-pointer"
+              onClick={() => handleImageClick(image)}
             >
               <Image
                 src={imageUrls[image.url as keyof typeof imageUrls]}
@@ -41,6 +59,7 @@ const SectionGallery: React.FC<SectionGalleryProps> = ({ gallery }) => {
                 style={{ height: "auto" }}
                 width={0}
                 height={0}
+                draggable={false}
               />
               <div className="absolute bottom-2 right-2 bg-black text-white text-sm px-2 py-1 rounded-lg opacity-75">
                 AI Generated
@@ -49,6 +68,13 @@ const SectionGallery: React.FC<SectionGalleryProps> = ({ gallery }) => {
           ))}
         </div>
       </div>
+      {selectedImage && (
+        <ImageModal
+          imageUrl={imageUrls[selectedImage.url as keyof typeof imageUrls]}
+          alt={selectedImage.alt}
+          onClose={handleCloseModal}
+        />
+      )}
     </div>
   );
 };
