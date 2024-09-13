dc5c81ebaa4ad3f1b77fd0617dbadce03556f19e
Multyp <miguel.dasilva2004@hotmail.com>
2024-09-13 22:19:36 +0200

    edit: moved css for filestructure of project, moved navbar out of main container

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -5,8 +5,8 @@ import React from "react";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
+      <Navbar />
       <main className="min-h-screen min-w-full bg-gray-100 text-black">
-        <Navbar />
         {children}
       </main>
       <Footer />
