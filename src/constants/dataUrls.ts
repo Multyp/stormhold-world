c096b4d06ffce4d1d691fb1d9ef9bf8bfe4b15bd
Multyp <miguel.dasilva2004@hotmail.com>
2024-09-22 17:27:21 +0200

    fix: data url for characters in env to fix prod

--- src/constants/dataUrls.ts
+++ src/constants/dataUrls.ts
@@ -1,6 +1,6 @@
 const dataUrls = {
   history: "src/data/history",
-  characters: "http://141.145.194.101:8000/characters",
+  characters: `${process.env.NEXT_PUBLIC_API_LINK}/characters`,
   locations: "src/data/locations",
 };
 
