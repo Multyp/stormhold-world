01ce36ce08fc0279b627d85851b4c6a32ca8fe78
Multyp <miguel.dasilva2004@hotmail.com>
2024-09-29 10:31:58 +0200

    fix: localstorage is not defined - build error

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -8,23 +8,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState<Theme>(
-    localStorage.getItem("theme") as Theme,
-  );
+  const [theme, setTheme] = useState<Theme>("light");
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem("theme") as Theme;
-    if (themesList.includes(savedTheme)) {
-      setTheme(savedTheme);
-      document.documentElement.classList.add(savedTheme);
-    } else {
-      setTheme("light");
-      document.documentElement.classList.add("light");
+    if (typeof window !== "undefined") {
+      // Only run this code on the client side - Fix for build error "localstorage is not defined"
+      const savedTheme = localStorage.getItem("theme") as Theme;
+      if (themesList.includes(savedTheme)) {
+        setTheme(savedTheme);
+        document.documentElement.classList.add(savedTheme);
+      } else {
+        setTheme("light");
+        document.documentElement.classList.add("light");
+      }
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem("theme", theme);
+    if (typeof window !== "undefined") {
+      // Only access localStorage on the client side - same issue
+      localStorage.setItem("theme", theme);
+    }
   }, [theme]);
 
   const handleThemeChange = () => {
@@ -41,7 +45,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Navbar />
       <button
         onClick={handleThemeChange}
-        className={`fixed bottom-4 left-4 z-50 p-2 rounded-full bg-primary text-primary border ${theme === "light" ? "border-gray-900 " : "border-primary"} shadow-lg transition-all duration-300 hover:scale-110 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary`}
+        className={`fixed bottom-4 left-4 z-50 p-2 rounded-full bg-primary text-primary border ${
+          theme === "light" ? "border-gray-900 " : "border-primary"
+        } shadow-lg transition-all duration-300 hover:scale-110 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary`}
         aria-label="Toggle theme"
       >
         <FontAwesomeIcon
